7a7cec5f0dfe93a6dfac4e83658e0b85d86ab418
Jonni Lundy <jonathan.lundy@liferay.com>
2018-02-28 11:57:30 -0800

    Adds upload to shared volume and db

--- uploader/index.js
+++ uploader/index.js
@@ -1,14 +1,30 @@
 const serveIndex = require('serve-index');
 const express = require('express');
+const WeDeploy = require('wedeploy');
 const multer = require('multer');
 const storage = multer.diskStorage({
   filename: (req, file, cb) => cb(null, file.originalname),
-  destination: (req, file, cb) => cb(null, './public/files'),
+  destination: (req, file, cb) => cb(null, './public/images'),
 })
 const upload = multer({storage});
 const app = express();
-app.post('/upload', upload.single('file'), (req, res) => {
-  res.redirect(`${req.protocol}://${req.get('host')}/files`);
+app.post('/upload', upload.single('image'), (req, res) => {
+  res.redirect(`${req.protocol}://${req.get('host')}/images`);
+
+  WeDeploy
+    .data('data-forsale.wedeploy.sh')
+    .create('listings', {
+      "title": req.body.title,
+      "description": req.body.description,
+      "price": req.body.price,
+      "filename": req.file.filename
+    })
+    .then(function(succ){
+      console.log(succ)
+    })
+    .catch(function(err){
+      console.log(err)
+    });
 });
 app.use(express.static(__dirname + '/public'));
 app.use(serveIndex('public', {'icons': true}));
